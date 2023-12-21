ab32a3a8edbdc43ce1369a9bc9dc9d5fe4c6dd15
ninaraynor <ninaraynor@gmail.com>
2023-12-21 14:39:41 -0500

    concerts schema

---
+++ models/concert.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const concertSchema = new Schema({
+  artistOrBand: { type: Schema.Types.ObjectId, ref: 'ArtistOrBand' },
+  venue: String,
+  date: Date,
+  setlist: { 
+    type: String 
+},
+  rating: Number,
+  review: { type: Schema.Types.ObjectId, ref: 'ConcertReview' },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Concert', concertSchema);
